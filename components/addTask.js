4ef39e41b31ae0c7bda8c8f8f7ddc1323e5cc264
Caleb Parrra <mccalebyt@gmail.com>
2022-03-29 10:07:18 -0500

    listando desde local storage

--- components/addTask.js
+++ components/addTask.js
@@ -2,36 +2,45 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento) =>{
-    const list = document.querySelector('[data-list]');
-    const task = createTask(evento);
-    list.appendChild(task);
-  }
-  
-    const createTask = (evento) => {
-    
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("tasks"))  || [];
-
+    const list = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const calendar = document.querySelector("[data-form-date]");
+     
+    const taskList = JSON.parse(localStorage.getItem("tasks"))  || [];
+
+ 
     const value = input.value;
     const date = calendar.value;
+    input.value = '';
+    calendar.value= "";
+  
 
     const dateFormat = (moment(date).format("DD/MM/YYYY"));
-    console.log(dateFormat);
     
-    const task = document.createElement('li');
-    task.classList.add('card');
-    input.value = '';
-  
-    const taskContent = document.createElement('div');
-  
+
     const taskObj = {
-      value,
-      dateFormat
+        value,
+        dateFormat
     }
+
     taskList.push(taskObj);
     localStorage.setItem("tasks", JSON.stringify(taskList));
+
+
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
+}
+  
+
+export const createTask = ({value, dateFormat}) => {
+
+    const task = document.createElement('li');
+    task.classList.add('card');
+   
+  
+    const taskContent = document.createElement('div');
   
     const dateElement = document.createElement("span");
     dateElement.innerHTML = (dateFormat);
@@ -46,4 +55,4 @@ export const addTask = (evento) =>{
     task.appendChild(dateElement);
     task.appendChild(deleteIcon());
     return task;
-  };
+};
